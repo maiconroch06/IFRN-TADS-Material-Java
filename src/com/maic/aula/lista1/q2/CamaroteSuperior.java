e90248d2aaf9757e9e199bb7c4911453fde8075e
Maicon_Rocha <maiconpablo588@gmail.com>
2025-09-29 21:20:00 -0300

    up new classs

--- src/com/maic/aula/lista1/q2/CamaroteSuperior.java
+++ src/com/maic/aula/lista1/q2/CamaroteSuperior.java
@@ -3,7 +3,9 @@ package com.maic.pooprojeto.lista1.q2;
 public class CamaroteSuperior extends VIP {
     private double adicionalSuperior;
     
+    double valor = imprimeValor();
+    
     public void imprimevalorVIP(){
-        return super.imprimeValor();IP + adicionalSuperior;
+        return  + adicionalSuperior;
     }
 }
